cb643c10b3599901731d02bde9f82478b4d8503e
Dsackler <dsackler@lion.lmu.edu>
2024-08-06 14:59:13 -0700

    Added struct to main

--- rust_single_layer_perceptron_classification/src/main.rs
+++ rust_single_layer_perceptron_classification/src/main.rs
@@ -1,10 +1,36 @@
+use ndarray::array;
 use ndarray::Array2;
+use rand::Rng;
 
 mod create_dataset;
+// mod initialize_params;
 mod layer_size;
 
 fn main() {
-    let data = create_dataset::create_dataset();
-    println!("{:?}", data.1);
-    // let size = layer_sizes(X, Y)
+    let (X, Y) = create_dataset::create_dataset();
+    println!("{:?}", X.len());
+    // Length of X = 2, Length of Y = 1.
+
+    let params = initialize_params();
+    println!("{:?}", params.W);
+}
+
+struct Parameters {
+    W: Array2<f64>,
+    b: Array2<f64>,
+}
+
+fn initialize_params() -> Parameters {
+    /*
+    Returns:
+    params -- struct containing parameters:
+                    W -- weight matrix of shape (n_y, n_x)
+                    b -- bias value set as a vector of shape (n_y, 1)
+     */
+    let mut rng = rand::thread_rng();
+    let W = array![[rng.gen(), rng.gen()]];
+    let b = array![[0.0]];
+
+    let parameters = Parameters { W: W, b: b };
+    parameters
 }
